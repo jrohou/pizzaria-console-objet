b565ea54971b485a5dd982f46aef876de1c06d84
Julien <julien.rohou@laposte.net>
2017-09-21 16:57:09 +0200

    Added menu and add new pizza

--- src/fr/pizzeria/console/Pizza.java
+++ src/fr/pizzeria/console/Pizza.java
@@ -2,15 +2,24 @@ package fr.pizzeria.console;
 
 public class Pizza {
 	
+	static int count = 0;
 	int id;
 	String code;
 	String nom;
 	double prix;
 	
+
+
 	public Pizza (String code, String nom, double prix) {
 		this.code = code;
 		this.nom = nom;
 		this.prix = prix;
+		id = count++;
+	}
+	
+	@Override
+	public String toString() {
+		return "Pizza [Identifiant=" + id + ", Code de la Pizza=" + code + ", Nom de la pizza=" + nom + ", Prix de la pizza=" + prix + "]";
 	}
 
 }
--- src/fr/pizzeria/console/PizzeriaAdminConsoleApp.java
+++ src/fr/pizzeria/console/PizzeriaAdminConsoleApp.java
@@ -1,5 +1,9 @@
 package fr.pizzeria.console;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class PizzeriaAdminConsoleApp {
 
 	public static void main(String[] args) {
@@ -20,6 +24,85 @@ public class PizzeriaAdminConsoleApp {
 		Pizza pizzanb6 = new Pizza("SAV","La savoyarde",13.00);
 		Pizza pizzanb7 = new Pizza("ORI","L'orientale",13.50);
 		Pizza pizzanb8 = new Pizza("IND","L'indienne",14.00);
+		
+		ArrayList<Pizza> pizzas = new ArrayList<Pizza>();
+		pizzas.add(pizzanb1);
+		pizzas.add(pizzanb2);
+		pizzas.add(pizzanb3);
+		pizzas.add(pizzanb4);
+		pizzas.add(pizzanb5);
+		pizzas.add(pizzanb6);
+		pizzas.add(pizzanb7);
+		pizzas.add(pizzanb8);
+		
+		
+		
+		Scanner sc = new Scanner(System.in);
+		
+
+		int rep = 0;
+		do {
+			System.out.println("Saisir un des numéros pour accéder aux fonctionnalités");
+			rep = sc.nextInt();
+			if (rep == 1) {
+				
+				System.out.println("Liste des pizzas");
+				
+				for(Pizza pizza: pizzas){
+					System.out.println(pizza);
+				}
+			}
+			else if (rep == 2) {
+				
+				System.out.println("Ajout d'une nouvelle pizza");
+				String newcode;
+				String newnom;
+				double newprix;
+				
+				do {
+				
+						System.out.println("Veuillez insérer le code la pizza");
+				
+							newcode = sc.nextLine();
+							
+							do {
+							
+								System.out.println("Veuillez insérer le nom de la pizza");
+								
+									newnom = sc.nextLine();
+									
+									do {
+									
+										System.out.println("Veuillez insérer le prix de la pizza");
+										
+											newprix = sc.nextDouble();
+											
+											
+											Pizza nouveau = new Pizza(newcode,newnom,newprix);
+											pizzas.add(nouveau);
+											
+										}while(newprix<0);
+								
+								}while(newnom=="");
+						
+					}while(newcode=="");									
+
+			}
+			else if (rep == 3) {
+				
+				System.out.println("Mise à jour d'une pizza");
+			}
+			else if (rep == 4) {
+				
+				System.out.println("Suppression d'une pizza");
+			}
+			else if (rep == 99) {
+				System.out.println("Au revoir ");
+			}
+			
+		} while (rep != 1 && rep!=2 && rep!=3 && rep!=4 && rep!=99 );
+		
+		
 		
 		
 
